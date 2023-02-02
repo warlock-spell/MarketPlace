531a593f21a2d380844f6d8169fe515eedc95916
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-02 20:22:28 +0530

    add landing page

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+# @Project:     HardwareHive
+# @Filename:    urls.py.py
+# @Author:      Daksh
+# @Time:        02-02-2023 07:46 pm
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    ]
