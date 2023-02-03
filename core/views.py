f53c3837f5ef1be1afe0d86a62528ee8f866baab
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-03 23:51:57 +0530

    show items on landing page

--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from item.models import Item, Category
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    context = {'items': items, 'categories': categories}
+    return render(request, 'core/index.html', context)
 
 
 def contact(request):
@@ -21,6 +25,7 @@ def terms(request):
 
 def about(request):
     return render(request, 'core/about.html')
+
 
 def careers(request):
     return render(request, 'core/careers.html')
