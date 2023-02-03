3fd97389fda7b5e9916f72feb1cd3f5f6ef1689f
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-03 23:23:27 +0530

    add Item model

--- item/admin.py
+++ item/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category
+from .models import Category, Item
 
 admin.site.register(Category)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'is_sold', 'created_by', 'created_at')
+    list_filter = ('is_sold', 'category', 'created_by')
+    search_fields = ('name', 'description', 'category')
+    readonly_fields = ('created_at',)
+
+
+admin.site.register(Item, ItemAdmin)
--- item/models.py
+++ item/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -9,6 +10,20 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
         ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_sold = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
