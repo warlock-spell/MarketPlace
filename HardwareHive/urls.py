a4c6d4609516cbb2cac7dff2da10a712534e923e
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-04 03:31:26 +0530

    add app - category

--- HardwareHive/urls.py
+++ HardwareHive/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls'), name='core'),
     path('item/', include('item.urls'), name='item',),
+    path('category/', include('category.urls'), name='category',),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ category/urls.py
@@ -0,0 +1,13 @@
+# @Project:     HardwareHive
+# @Filename:    urls.py
+# @Author:      Daksh
+# @Time:        04-02-2023 03:30 am
+
+from django.urls import path
+from . import views
+
+app_name = 'category'
+
+urlpatterns = [
+
+]
