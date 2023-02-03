e01a8d71132317216cb5f36dd49ac95d2f9c6b66
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-03 22:20:00 +0530

    add Category model

--- item/models.py
+++ item/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
