afcf2eb1fbe045a77f91fcf02a48ef06302d4f90
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-04 03:27:40 +0530

    add product recommendations

--- item/views.py
+++ item/views.py
@@ -7,5 +7,9 @@ from .models import Item
 
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    context = {'item': item, }
+    # fetching related items excluding the current item
+    related_items = Item.objects.filter(
+        category=item.category, is_sold=False
+    ).exclude(pk=pk)[:3]
+    context = {'item': item, 'related_items': related_items, }
     return render(request, 'item/item-detail.html', context)
