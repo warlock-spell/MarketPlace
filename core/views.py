35f5a0aa634f8354f390147921cbcc94927a3a7d
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-19 16:07:01 +0530

    save signup form and redirect

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Item, Category
 from .forms import SignupForm
 
@@ -28,6 +28,16 @@ def about(request):
 
 
 def signup(request):
-    form = SignupForm()
-    context = {'form': form}
-    return render(request, 'core/signup.html', context)
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
