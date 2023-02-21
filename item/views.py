ba2e7f74e0d09f0407944fd96a0469b4fbbea564
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-21 22:18:58 +0530

    add item search

--- item/views.py
+++ item/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .models import Item
+from category.models import Category
 from .forms import NewItemForm, EditItemForm
 
 
 # Create your views here.
 
 def browse_items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
     items = Item.objects.filter(is_sold=False)
-    context = {'items': items, }
-    return render(request, 'item/browse-items.html', context)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/browse-items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
+    })
 
 
 def item_detail(request, pk):
