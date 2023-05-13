2b4aa8dd2f5becb748090bd108cd433eae64dda1
Daksh Gaur <hi@daksh.fyi>
2023-05-14 02:52:23 +0530

    filter and display the 6 latest items based on their creation date

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from .forms import SignupForm
 # Create your views here.
 
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
+    items = Item.objects.filter(is_sold=False).order_by('-created_at')[0:6]
     categories = Category.objects.all()
     context = {'items': items, 'categories': categories}
     return render(request, 'core/index.html', context)
